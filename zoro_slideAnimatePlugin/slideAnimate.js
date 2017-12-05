979c4ce6b1701a684a46670ee9ca41f174a31f54
haihanh2@163.com <you@example.com>
2017-12-05 16:15:52 +0800

    recode completed

--- zoro_slideAnimatePlugin/slideAnimate.js
+++ zoro_slideAnimatePlugin/slideAnimate.js
@@ -10,27 +10,36 @@
  * 包含三个参数wrapName，direction，animateSpeed
  */
 function SlideAnimate(condition) {
-    this.wrapName = condition.wrapName || '.slideAnimate';
     this.direction = /left|right|top|bottom/.test(condition.animateDirection) ? condition.animateDirection : 'left';
-    this.animateSpeed = condition.animateSpeed || -2;
+    this.animateSpeed = condition.animateSpeed || 2;
+    this.resourcesArr = condition.resourcesArr;
     this.textMode = condition.textMode || false;
     this.textModeStyle = condition.textModeStyle;
-    this.directionCssObj = {};
-    this.dragPosStart = 0;
-    this.timer = null;
     this.init();
 }
 
 SlideAnimate.prototype = {
     constructor: SlideAnimate,
     init: function () {
+        this.initHtml();
         this.handleCopyHtml();
         this.handleInterval("add");
         this.handleMouseEvent();
     },
+    initHtml: function () {
+        var htmlTemplate = '<ul>';
+        this.resourcesArr.forEach( function(item) {
+            htmlTemplate += this.textMode
+                ? '<li><p>'+ item +'</p></li>'
+                : '<li><img src='+ item +'/></li>';
+        }.bind(this));
+        htmlTemplate += '</ul>';
+
+        $("#slideAnimate").append(htmlTemplate);
+    },
     handleCopyHtml: function(){
         //复制html节点
-        this.oUl = $(this.wrapName).find("ul");
+        this.oUl = $("#slideAnimate").find("ul");
         this.oUl.append(this.oUl.html());
         this.oLi = this.oUl.find("li");
 
@@ -40,13 +49,13 @@ SlideAnimate.prototype = {
                 direction: "left",
                 directionProp: "width"
             };
-            $(this.wrapName).find("li").css({"float": "left", "height": "100%"}).find("img").css("width", "100%");
+            $("#slideAnimate").find("li").css({"float": "left", "height": "100%"}).find("img").css("width", "100%");
         } else {
             this.directionCssObj = {
                 direction: "top",
                 directionProp: "height"
             };
-            $(this.wrapName).find("li").css("height", this.oUl.height() + "px").find("img").css("height", "100%");
+            $("#slideAnimate").find("li").css("height", this.oUl.height() + "px").find("img").css("height", "100%");
         }
 
         //文字模式下，渲染自定义文字的样式
@@ -70,27 +79,28 @@ SlideAnimate.prototype = {
                 e.preventDefault();
                 $(_this.oUl).addClass("grab");
 
-                var propName = "pageX";
-                /top|bottom/.test(_this.direction) ? propName = "pageY" : propName;
+                var propName = /top|bottom/.test(_this.direction) ? "pageY" : "pageX";
                 _this.dragPosStart = e[propName];
             },
             "mouseup": function (e) {
                 e.preventDefault();
                 $(_this.oUl).removeClass("grab");
 
-                var propName = "pageX";
-                /top|bottom/.test(_this.direction) ? propName = "pageY" : propName;
-                var ulPropNumber = parseInt($(_this.oUl).css(_this.directionCssObj.direction).replace(/[^0-9|-]/gi, ""));
+                var propName = /top|bottom/.test(_this.direction) ? "pageY" : "pageX";
+                var ulPropNumber = parseInt( $(_this.oUl).css(_this.directionCssObj.direction) );
                 var propHalfLength = _this.oLi[_this.directionCssObj.directionProp]() * (_this.oLi.length / 2);
                 var ulComputedNumber = ulPropNumber + (e[propName] - _this.dragPosStart);
+                var ulSlide = 0;
 
                 if (ulComputedNumber > 0) {
-                    $(_this.oUl).css(_this.directionCssObj.direction, "0px");
+                    ulSlide = 0;
                 } else if (Math.abs(ulComputedNumber) > propHalfLength) {
-                    $(_this.oUl).css(_this.directionCssObj.direction, -propHalfLength + "px");
+                    ulSlide = -propHalfLength;
                 } else {
-                    $(_this.oUl).css(_this.directionCssObj.direction, ulComputedNumber + "px");
+                    ulSlide = ulComputedNumber;
                 }
+
+                $(_this.oUl).css(_this.directionCssObj.direction, ulSlide);
             }
         });
     },
@@ -99,11 +109,11 @@ SlideAnimate.prototype = {
      * @param type  add or cancel
      */
     handleInterval: function(type) {
-        if( type === 'add' ){
-            this.timer = setInterval(this.handleAnimate(this), 20)
-        }else{
-            clearInterval(this.timer);
-        }
+        type === 'add'
+            ? this.timer = setInterval( this.handleAnimate(this), 20)
+            : clearInterval(this.timer);
+
+            //this.timer = requestAnimationFrame(this.handleAnimate(this));
     },
     /**
      * 处理动画操作
@@ -113,18 +123,18 @@ SlideAnimate.prototype = {
     handleAnimate: function (_this) {
         return function () {
             if (_this.directionCssObj) {
-                var propNumber = parseInt(_this.oUl.css(_this.directionCssObj.direction).replace(/[^0-9]/gi, ""));
+                var propNumber = _this.oUl.css(_this.directionCssObj.direction).replace(/[^0-9]/g, "");
 
-                if (_this.animateSpeed > 0) {
-                    //大于0方向向下或者向右
+                if (/right|bottom/.test(_this.direction)) {
+                    //方向向下或者向右
                     propNumber === 0
-                        ? _this.oUl.css(_this.directionCssObj.direction, -_this.oUl[_this.directionCssObj.directionProp]() / 2 + "px")
+                        ? _this.oUl.css(_this.directionCssObj.direction, - _this.oUl[_this.directionCssObj.directionProp]() / 2 + "px")
                         : _this.oUl.css(_this.directionCssObj.direction, "+=" + _this.animateSpeed + "px");
                 } else {
-                    //小于0方向向上或者向左
+                    //方向向上或者向左
                     propNumber >= _this.oLi[_this.directionCssObj.directionProp]() * (_this.oLi.length / 2)
                         ? _this.oUl.css(_this.directionCssObj.direction, "0px")
-                        : _this.oUl.css(_this.directionCssObj.direction, "+=" + _this.animateSpeed + "px");
+                        : _this.oUl.css(_this.directionCssObj.direction, "+=" + -_this.animateSpeed + "px");
                 }
             } else {
                 console.warn("directionCssObj不能为空!");
